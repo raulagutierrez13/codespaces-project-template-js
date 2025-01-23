ab2646715f20076425a854a6fcd71c7e7dd3cc99
raulagutierrez13 <raul.a.gutierrez@accenture.com>
2025-01-23 11:03:53 +0000

    myvalues

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
-  medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  name: "Raul Gutierrez",
+  title: "Quality Assurance Archt Manager",
+  email: "Raul.a.gutierrez@accenture.com",
+  gitHub: "raulagutierrez13",
+  instagram: "IdontHaveInstagram",
+  linkedIn: "raulagutierrez13",
+  medium: "raula.gutierrez",
+  twitter: "raulagutierrez13",
+  youTube: "raulagutierrez13",
 };
 
 const primaryColor = "#4E567E";
